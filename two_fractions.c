aa550b8e2730f978719d45e5ad9048c1136d9ff8
na-and <75084007+na-and@users.noreply.github.com>
2021-02-09 22:56:09 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -1,1 +1,24 @@
 //WAP to find the sum of two fractions.
+#include <stdio.h>
+int main()
+{
+int a, b,c,d,x,y,i,gcd;
+printf("\nEnter the numerator for 1st number :\t");
+scanf("%d",&a);
+printf("\nEnter the denominator for 1st number :\t");
+scanf("%d",&b);
+printf("\nEnter the numerator for 2nd number :\t");
+scanf("%d",&c);
+printf("\nEnter the denominator for 2nd number :\t");
+scanf("%d",&d);
+x=(a*d)+(b*c);
+y=b*d; 
+for(i=1; i <= x && i <= y; ++i)
+{
+if(x%i==0 && y%i==0)
+gcd = i;
+}
+printf("\nThe added fraction is %d/%d ",x/gcd,y/gcd);
+printf("\n");
+return 0;
+}
