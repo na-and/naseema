8ee9ed70ea31c47bcd4dbd0425d7cbc27b3961a9
na-and <75084007+na-and@users.noreply.github.com>
2021-02-15 22:13:20 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -1,24 +1,30 @@
 //WAP to find the sum of two fractions.
-#include <stdio.h>
+#include<stdio.h>
+#include<conio.h>
+typedef struct
+{
+        int num;
+        int deno;
+     
+}
+Fract;
+Fract sum(Fract,Fract); 
 int main()
 {
-int a, b,c,d,x,y,i,gcd;
-printf("\nEnter the numerator for 1st number :\t");
-scanf("%d",&a);
-printf("\nEnter the denominator for 1st number :\t");
-scanf("%d",&b);
-printf("\nEnter the numerator for 2nd number :\t");
-scanf("%d",&c);
-printf("\nEnter the denominator for 2nd number :\t");
-scanf("%d",&d);
-x=(a*d)+(b*c);
-y=b*d; 
-for(i=1; i <= x && i <= y; ++i)
-{
-if(x%i==0 && y%i==0)
-gcd = i;
+     int num1,deno1,num2,deno2;
+     printf("Enter fraction 1: numerator denominator:");
+     scanf("%d%d",&num1,&deno1);
+     printf("Enter fraction 2:numerator denominator:");
+     scanf("%d%d",&num2,&deno2);
+     Fract f1={num1, deno1}; 
+     Fract f2 ={num2, deno2};
+     Fract result = sum(f1, f2);
+     printf("Result=%d/%d",result.num,result.deno);  
+     getch();
+     return 0;
 }
-printf("\nThe added fraction is %d/%d ",x/gcd,y/gcd);
-printf("\n");
-return 0;
+Fract sum(Fract f1, Fract f2)
+{
+     Fract result={(f1.num * f2.deno) + (f2.num * f1.deno), f1.deno * f2.deno};
+     return result; 
 }
