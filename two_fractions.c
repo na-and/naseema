7590907258e5a364fc2a36f17ff87e64504df1a6
na-and <75084007+na-and@users.noreply.github.com>
2021-02-16 00:31:23 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -2,29 +2,44 @@
 #include<stdio.h>
 #include<conio.h>
 typedef struct
-{
+     {
         int num;
         int deno;
      
-}
-Fract;
-Fract sum(Fract,Fract); 
-int main()
+      }Fract;
+ 
+   Fract sum(Fract,Fract);
+ float input()
 {
+float number;
+scanf("%f",&number);
+return number
+}
+float output(num,deno)
+{
+return ("Result=%d%d",num,deno);  //display the result
+}
+   int main()
+    {
      int num1,deno1,num2,deno2;
      printf("Enter fraction 1: numerator denominator:");
-     scanf("%d%d",&num1,&deno1);
+ num1 = input();
+deno1=input();
      printf("Enter fraction 2:numerator denominator:");
-     scanf("%d%d",&num2,&deno2);
-     Fract f1={num1, deno1}; 
-     Fract f2 ={num2, deno2};
-     Fract result = sum(f1, f2);
-     printf("Result=%d/%d",result.num,result.deno);  
+ num2=input();
+deno2=input();
+    
+     Fract f1={num1, deno1}; // fraction 1
+     Fract f2 ={num2, deno2};//fraction 2
+     Fract result = sum(f1, f2);//sum the fraction
+ display=output(result.num,result.deno);
+printf(display)
      getch();
      return 0;
-}
-Fract sum(Fract f1, Fract f2)
-{
+    }
+   Fract sum(Fract f1, Fract f2)
+    {
      Fract result={(f1.num * f2.deno) + (f2.num * f1.deno), f1.deno * f2.deno};
-     return result; 
-}
+     return result;
+ 
+   }
